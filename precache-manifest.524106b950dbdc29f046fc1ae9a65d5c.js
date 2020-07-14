578c93995e2d1d2cb7ab468f5375cb82bc95a3cb
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-13 22:28:58 -0700

    Updates

---
+++ precache-manifest.524106b950dbdc29f046fc1ae9a65d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91757cd8cdb31b80558e02a45baae3a0",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "e79e8fd78bee343f2bf1",
+    "url": "/react-todo/static/css/main.ebb28d3c.chunk.css"
+  },
+  {
+    "revision": "1751c770828572cb54d4",
+    "url": "/react-todo/static/js/2.fbf9a9c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.fbf9a9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79e8fd78bee343f2bf1",
+    "url": "/react-todo/static/js/main.c9303624.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
